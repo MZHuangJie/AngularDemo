a884eaf88b7eb1284db2b92e08feab1a96fe281a
黄杰 <jie.huang@jimicn.com>
2019-09-05 17:12:11 +0800

    英雄列表

---
+++ app/src/app/hero/hero.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { Hero } from './hero';
+import { HEROES } from './mock-heroes';
+
+@Component({
+  selector: 'app-hero',
+  templateUrl: './hero.component.html',
+  styleUrls: ['./hero.component.css']
+})
+export class HeroComponent implements OnInit {
+  heroes = HEROES;
+  selectedHero: Hero;
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
