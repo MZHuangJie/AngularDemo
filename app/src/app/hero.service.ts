c7938b83c1f5ef102be00e490801bb71703e889e
黄杰 <jie.huang@jimicn.com>
2019-09-24 18:32:50 +0800

    至HttpClient.get返回响应数据

--- app/src/app/hero.service.ts
+++ app/src/app/hero.service.ts
@@ -3,17 +3,23 @@ import { Hero } from './hero/hero';
 import { HEROES } from './hero/mock-heroes';
 import { Observable, of } from 'rxjs';
 import { MessagesService } from './messages.service';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
 
-  constructor(private messageService: MessagesService) { }
-
+  constructor(
+    private http: HttpClient,
+    private messageService: MessagesService
+    ) {}
+  private heroesUrl = 'api/heroes';
+  private log(message: string) {
+    this.messageService.add(`HeroService: ${message}`);
+  }
   getHeroes(): Observable<Hero[]> {
-    this.messageService.add('HeroService: fetched heroes');
-    return of(HEROES);
+    return this.http.get<Hero[]>(this.heroesUrl);
   }
 
   getHero(id: number): Observable<Hero> {
