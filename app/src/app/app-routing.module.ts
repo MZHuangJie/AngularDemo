fa51790cf374ed6dc7a26d9e55e1f90126a768bf
黄杰 <jie.huang@jimicn.com>
2019-11-18 13:45:57 +0800

    emmmm

--- app/src/app/app-routing.module.ts
+++ app/src/app/app-routing.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { HeroComponent } from './hero/hero.component';
+import { HeroesComponent } from './hero/hero.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
-  { path: 'heroes', component: HeroComponent },
+  { path: 'heroes', component: HeroesComponent },
   { path: 'dashboard', component: DashboardComponent },
   { path: 'detail/:id', component: HeroDetailComponent }
 ];
